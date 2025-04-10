b4b8da8c0184cc3c7d55ed1f395ef5a50a7c52e4
albertito1985 <anulo@live.se>
2025-04-10 12:31:48 +0200

    Frågor svarade

--- Lexicon-Uppgift3/Program.cs
+++ Lexicon-Uppgift3/Program.cs
@@ -6,7 +6,20 @@ using Lexicon_Uppgift3.Menus;
 namespace Lexicon_Uppgift3;
 
 internal class Program
-{
+{   
+    //QUESTIONS
+
+    //Vad händer om du försöker lägga till en Car i en lista av Motorcykel?
+        //Då får man ett fel
+    
+    //Vilken typ bör en lista vara för att rymma alla fördonstyper?
+        //Vehicle
+
+    //Kommer du åt metoden Clean() från en lista med typen List<Vehicle>?
+        //Nej
+
+    //Vad är fördelarna med att använda interfaces istället av arv?
+        //Att man kan, i en och samma kod bit, använda olika Klasser med samma gemensamma metoder.
     static void Main(string[] args)
     {
         //Del 1
